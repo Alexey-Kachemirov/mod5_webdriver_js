72d495b0a1ec5d7673ed2c71c5ac19560d5ad990
MINSK\Alexey_Kachemirov <alexey_kachemirov@epam.com>
2018-07-20 12:03:08 +0300

    updated task using promises + some new api requests

--- index.js
+++ index.js
@@ -1,46 +1,53 @@
 const creds = require("./creds");
-
-var webdriver = require("selenium-webdriver"),
+const webdriver = require("selenium-webdriver"),
     By = webdriver.By,
     until = webdriver.until;
 
-function createDriver() {
-    var driver = new webdriver.Builder()
-        .usingServer('http://localhost:4444/wd/hub')
-        .withCapabilities(webdriver.Capabilities.chrome())
-        .build();
-    return driver;
-}
+let driver = new webdriver.Builder()
+.usingServer('http://localhost:4444/wd/hub')
+.withCapabilities(webdriver.Capabilities.chrome())
+.build();
 
-var browser = createDriver();
-
-browser.get('http://localhost:7000/');
-browser.manage().timeouts().implicitlyWait(5000);
-browser.findElement(By.id("userName")).clear();
-browser.findElement(By.css("#userName")).sendKeys(creds.userName);
-browser.findElement(By.name("firstName")).clear();
-browser.findElement(By.xpath(".//input[@id='firstName']")).sendKeys(creds.firstName);
-browser.findElement(By.css("#lastName")).clear();
-browser.findElement(By.css("#lastName")).sendKeys(creds.lastName);
-browser.findElement(By.css("#email")).clear();
-browser.findElement(By.css("#email")).sendKeys(creds.email);
-browser.findElement(By.css("#role")).sendKeys(creds.role);
-browser.findElement(By.xpath(".//span[text()='Sign in']")).click();
-browser.wait(until.elementLocated(By.css("div[class*='dropdown-menu_header']")));
-browser.findElement(By.css("div[class*='dropdown-menu_header']")).click();
-browser.findElement(By.linkText("Manage Attributes")).click();
-browser.findElement(By.partialLinkText("Add Attr")).click();
-browser.findElement(By.css("div[class*='with-field_root'] input[class*='text-input_input']")).sendKeys("!name_for_webdriverjs_ak_module");
-browser.findElement(By.css("textarea[class*='text-input_textarea']")).sendKeys("description_for_webdriverjs_ak_module");
-browser.findElement(By.xpath(".//span[text()='Please select...']")).click();
-browser.findElement(By.xpath(".//div[text()='Text']")).click();
-browser.findElement(By.className("_d7e6c8__checkbox_label")).click();
-browser.findElement(By.xpath(".//button[text()='Create']")).click();
-browser.findElement(By.css("div[class*='inline-notification_root__success']")).getText().then(function(txt){
-    console.log("Success creation message: " + txt);
-});
-browser.findElement(By.xpath(".//td[text()='!name_for_webdriverjs_ak_module']/..//i[@data-qa-id='delete']")).click();
-browser.findElement(By.xpath(".//button[text()='Yes']")).click();
-browser.wait(until.elementLocated(By.css("div[class*='inline-notification_root__success']")));
-browser.sleep(3000);
-browser.quit();
+driver.get('http://localhost:7000/')
+    .then (_ => driver.manage().window().maximize())
+    .then (_ => driver.manage().timeouts().implicitlyWait(5000))
+    .then (_ => driver.findElement(By.id("userName")).clear())
+    .then (_ => driver.findElement(By.css("#userName")).sendKeys(creds.userName))
+    .then (_ => driver.findElement(By.name("firstName")).clear())
+    .then (_ => driver.findElement(By.xpath(".//input[@id='firstName']")).sendKeys(creds.firstName))
+    .then (_ => driver.findElement(By.css("#lastName")).clear())
+    .then (_ => driver.findElement(By.css("#lastName")).sendKeys(creds.lastName))
+    .then (_ => driver.findElement(By.css("#email")).clear())
+    .then (_ => driver.findElement(By.css("#email")).sendKeys(creds.email))
+    .then (_ => driver.findElement(By.css("#role")).sendKeys(creds.role))
+    .then (_ => driver.findElement(By.xpath(".//span[text()='Sign in']")).isDisplayed())
+    .then (_ => driver.findElement(By.xpath(".//span[text()='Sign in']")).click())
+    .then (_ => driver.findElement(By.css("img[alt='Organics']")).isDisplayed())
+    .then (_ => driver.findElement(By.css("div[class*='dropdown-menu_header']")).isEnabled())
+    .then (_ => driver.findElement(By.css("div[class*='dropdown-menu_header']")).click())
+    .then (_ => driver.findElement(By.linkText("Manage Attributes")).isDisplayed())
+    .then (_ => driver.findElement(By.linkText("Manage Attributes")).click())
+    .then (_ => driver.findElement(By.partialLinkText("Add Attr")).click())
+    .then (_ => driver.findElement(By.css("div[class*='with-field_root'] input[class*='text-input_input']")).sendKeys("!name_for_webdriverjs_ak_module"))
+    .then (_ => driver.findElement(By.css("textarea[class*='text-input_textarea']")).sendKeys("description_for_webdriverjs_ak_module"))
+    .then (_ => driver.findElement(By.xpath(".//span[text()='Please select...']")).click())
+    .then (_ => driver.findElement(By.xpath(".//div[text()='Text']")).click())
+    .then (_ => driver.findElements(By.css("label[class*='checkboxContainer']")))
+    .then((elements) => {
+        const promises = elements
+            .map(element => {
+                return element.getText();
+            });
+            return Promise.all(promises);
+    })
+    .then (_ => driver.findElement(By.className("_d7e6c8__checkbox_label")).click())
+    .then (_ => driver.findElement(By.xpath(".//button[text()='Create']")).isSelected())
+    .then (_ => driver.findElement(By.xpath(".//button[text()='Create']")).click())
+    .then (_ => driver.findElement(By.css("div[class*='inline-notification_root__success']")).getText().then(function(txt){
+            console.log("Success creation message: " + txt);
+        }))
+    .then (_ => driver.findElement(By.xpath(".//td[text()='!name_for_webdriverjs_ak_module']/..//i[@data-qa-id='delete']")).click())
+    .then (_ => driver.findElement(By.xpath(".//button[text()='Yes']")).click())
+    .then (_ => driver.findElement(By.css("div[class*='inline-notification_root__success']")).isDisplayed())
+    .then (_ => driver.wait(until.elementLocated(By.css("div[class*='inline-notification_root__success']"))), 5000)
+    .then (_ => driver.quit());
